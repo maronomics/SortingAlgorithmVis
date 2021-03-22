e5dd54028d965c64128cc82f57e1f946034c69a4
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-22 21:51:15 +0500

    buttons added

--- sorted/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorted/src/SortingVisualizer/SortingVisualizer.jsx
@@ -35,7 +35,27 @@ export default class SortingVisualizer extends React.Component {
     }
 
     mergeSort(){
-
+        const animations = getMergeSortAnimations(this.state.array)
+        for (let i = 0; i < animations.length; i++){
+            const arrayBars = document.getElementsByClassName('array-bar')
+            const isColourChange = i % 3 !== 2
+            if (isColourChange){
+                const [barOneIdx, barTwoIdx] = animations[i]
+                const barOneStyle = arrayBars[barOneIdx].style 
+                const barTwoStyle = arrayBars[barTwoIdx].style 
+                const colour = i % 3 === 0 ? SECONDARY_COLOUR : PRIMARY_COLOUR 
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color 
+                    barTwoStyle.backgroundColor = color 
+                }, i * ANIMATION_SPEED_MS)
+            } else{
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i]
+                    const barOneStyle = arrayBars[barOneIdx].style 
+                    barOneStyle.height = '${newHeight}px'
+                }, i * ANIMATION_SPEED_MS)
+            }
+        }
     }
 
     bubbleSort(){
@@ -51,5 +71,38 @@ export default class SortingVisualizer extends React.Component {
     }
 
     testSortingAlgorithms(){
+        for(let i = 0; i < 100; i++){
+            const array = []
+            const length = randomIntFromInterval(1,1000)
+            for (let i = 0; i < length; i++){
+                array.push(randomIntFromInterval(-1000,1000))
+            }
+            const javaScriptSortedArray = array.slice().sort((a,b) => a - b)
+            const mergeSortedArray = getMergeSortAnimations(array.slice())
+            console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray))
+        }
+    }
 
+    render (){
+        const {array} = this.state 
+        return (
+            <div className = "array-container">
+                {array.map((value, idx) => (
+                    <div 
+                        className = "array-bar"
+                        key = {idx}
+                        style= {{
+                            backgroundColor: PRIMARY_COLOUR,
+                            height: '${value}px',
+                        }}></div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>BubbleSort</button>
+
+
+            </div>
+        )
     }
