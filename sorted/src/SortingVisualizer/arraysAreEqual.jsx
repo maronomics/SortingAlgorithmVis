ba56471b4c30a6c209e19a209761073604b59a1f
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-22 21:25:00 +0500

    arrayAreEqual function implemented

---
+++ sorted/src/SortingVisualizer/arraysAreEqual.jsx
@@ -0,0 +1,11 @@
+function arraysAreEqual(arrOne, arrTwo) {
+    if (arrOne.length !== arrTwo.length) {
+        return false;
+    }
+    for (let i = 0; i < arrOne.length; i++) {
+        if (arrOne[i] !== arrTwo[i]) {
+            return false;
+        }
+    }
+    return true;
+}
